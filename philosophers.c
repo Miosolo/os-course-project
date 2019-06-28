2572319193bfa49bf3960718d52701d21fd4aac9
Miosolo <xht2012.jn@gmail.com>
2019-06-29 02:16:43 +0800

    format

--- philosophers.c
+++ philosophers.c
@@ -12,54 +12,56 @@ pthread_mutex_t forks[5];  // 5 forks in a round
 
 // the activity of ith philosopher
 void *philo_act(void *intVal) {
-  int seat = *(int *)intVal;
-  srand(seat);  // init the random seed
-  for (int round = 1, dinnerCnt = 0; ;round++) {
-    char *thisGuy = philos[seat];
-    printf("[%d]/[%d] %s is thinking\n", dinnerCnt, round, thisGuy);
-    // sleep for 0~1s randomly to prevent race
-    usleep(rand() % 1000000); // us
-    printf("[%d]/[%d] %s becomes hungry\n", dinnerCnt, round, thisGuy);
+	int seat = *(int *)intVal;
+	srand(seat);  // init the random seed
+	for (int round = 1, dinnerCnt = 0;; round++) {
+		char *thisGuy = philos[seat];
+		printf("[%d]/[%d] %s is thinking\n", dinnerCnt, round, thisGuy);
+		// sleep for 0~1s randomly to prevent race
+		usleep(rand() % 1000000);  // us
+		printf("[%d]/[%d] %s becomes hungry\n", dinnerCnt, round, thisGuy);
 
-    // try to get the right fork
-    if (pthread_mutex_trylock(&forks[(seat + 1) % 5]) != SUCCESS) {
-      continue;  // go back thinking
-    }
-    printf("[%d]/[%d] %s gets the right fork\n", dinnerCnt, round, thisGuy);
-    // try to get the left fork
-    if (pthread_mutex_trylock(&forks[seat - 1]) != SUCCESS) {
-      (pthread_mutex_unlock(&forks[(seat + 1) % 5]));  // realease lock
-      continue;                                        // go back thinking
-    }
-    printf("[%d]/[%d] %s gets the left fork\n", dinnerCnt, round, thisGuy);
+		// try to get the right fork
+		if (pthread_mutex_trylock(&forks[(seat + 1) % 5]) != SUCCESS) {
+			continue;  // go back thinking
+		}
+		printf("[%d]/[%d] %s gets the right fork\n", dinnerCnt, round, thisGuy);
+		// try to get the left fork
+		if (pthread_mutex_trylock(&forks[seat - 1]) != SUCCESS) {
+			(pthread_mutex_unlock(&forks[(seat + 1) % 5]));  // realease lock
+			continue;                                        // go back thinking
+		}
+		printf("[%d]/[%d] %s gets the left fork\n", dinnerCnt, round, thisGuy);
 
-    // if get both forks
-    usleep(rand() % 1000000); // us
-    printf("[%d]/[%d] %s finishes %dth dinner\n", dinnerCnt, round, thisGuy, dinnerCnt++);
-    // release forks
-    pthread_mutex_unlock(&forks[(seat + 1) % 5]);
-    pthread_mutex_unlock(&forks[seat - 1]);
-  }
+		// if get both forks
+		usleep(rand() % 1000000);  // us
+		dinnerCnt++;
+		printf("[%d]/[%d] %s finishes %dth dinner\n", dinnerCnt, round, thisGuy,
+					 dinnerCnt);
+		// release forks
+		pthread_mutex_unlock(&forks[(seat + 1) % 5]);
+		pthread_mutex_unlock(&forks[seat - 1]);
+	}
 }
 
 int main() {
-  // init the mutex locks
-  pthread_mutexattr_t attr;
-  pthread_mutexattr_init(&attr);
-  for (int i = 0; i < 5; i++) {
-    pthread_mutex_init(&forks[i], &attr);
-  }
+	// init the mutex locks
+	pthread_mutexattr_t attr;
+	pthread_mutexattr_init(&attr);
+	for (int i = 0; i < 5; i++) {
+		pthread_mutex_init(&forks[i], &attr);
+	}
 
-  // invite philosophers
-  int params[] = {0, 1, 2, 3, 4, 5};  // philos' seats num
-  pthread_t tid;                      // thread id
-  for (int i = 1; i <= 5; i++) {
-    if (pthread_create(&tid, NULL, philo_act, (void *)&params[i]) != SUCCESS) {
-      perror("creating thread");
-      return 1;
-    }
-    printf("%s joined\n", philos[i]);
-  }
-  // join the last tid to prevent main() terminates
-  pthread_join(tid, NULL);
+	// invite philosophers
+	int params[] = {0, 1, 2, 3, 4, 5};  // philos' seats num
+	pthread_t tid;                      // thread id
+	for (int i = 1; i <= 5; i++) {
+		if (pthread_create(&tid, NULL, philo_act, (void *)&params[i]) != SUCCESS) {
+			perror("creating thread");
+			return 1;
+		}
+		printf("%s joined\n", philos[i]);
+	}
+	// join the last tid to prevent main() terminates
+	pthread_join(tid, NULL);
 }
--- pipe-chat.c
+++ pipe-chat.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 #define FAIL -1
 
 int main(void) {
-  // 2-way pipe, master proc writes on [0], 
+  // 2-way pipe, master proc writes on [0],
   // child procs write on [1]
   int pipefds[2];
   // init pipe
@@ -24,7 +24,7 @@ int main(void) {
         close(pipefds[0]);  // close the read side
         char msg[30];
         sprintf(msg, "Child process %d is sending a message!\n", i);
-        if (write(pipefds[1], msg, strlen(msg)) == FAIL){  // send the msg
+        if (write(pipefds[1], msg, strlen(msg)) == FAIL) {  // send the msg
           perror("sending msg");
           exit(1);
         }
@@ -40,11 +40,11 @@ int main(void) {
   }
 
   // read from pipe
-  close(pipefds[1]); // close the write side
+  close(pipefds[1]);  // close the write side
   char buf[100];
-  int len = read(pipefds[0], buf, 100); // receive msgs
-  buf[len] = 0; // end the string
+  int len = read(pipefds[0], buf, 100);  // receive msgs
+  buf[len] = 0;                          // end the string
   printf("received from child processes:\n%s", buf);
-  
+
   return 0;
 }
